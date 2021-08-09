39d82d0f35e9c93aa16df9d92847058fc9d5b2ea
Biplov <biplov.kc@checkout.com>
2021-08-09 11:04:05 +0200

    added threshold option fro failed request and http request duration

--- test.js
+++ test.js
@@ -1,10 +1,20 @@
 import http from "k6/http";
 import { check } from "k6";
+import { Rate } from "k6/metrics";
 
+const failures = new Rate("failed requests");
+
+export const options = {
+  thresholds: {
+    failed_requests: ["rate<=0"],
+    http_req_duration: ["p(95)<100"],
+  },
+};
 export default function () {
   const result = http.get("https://test-api.k6.io");
 
   check(result, {
     "Response code is 200": (r) => r.status === 200,
   });
+  failures.add(result.status != 200);
 }
