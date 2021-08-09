04ff520489d08396360450eb8a657170a71fc429
Biplov <biplov.kc@checkout.com>
2021-08-09 10:59:07 +0200

    added check for result

--- test.js
+++ test.js
@@ -1,5 +1,10 @@
 import http from "k6/http";
+import { check } from "k6";
 
 export default function () {
   const result = http.get("https://test-api.k6.io");
+
+  check(result, {
+    "Response code is 200": (r) => r.status === 200,
+  });
 }
